1088a39447c9d7fd91a5788e92d74d89a8ef00f1
Your Name <ahmedhamada333@gmail.com>
2017-10-14 19:19:38 +0200

    YOUR MESSAGE

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
-require_once __DIR__ . '/facebook/autoload.php'; // change path as needed
+/*require_once __DIR__ . '/facebook/autoload.php'; // change path as needed
 
 $fb = new \Facebook\Facebook([
   'app_id' => '{817858868393596}',
   'app_secret' => '{9e064077473efa4de495d4d98120a486}',
   'default_graph_version' => 'v2.10',
   //'default_access_token' => '{access-token}', // optional
-]);
+]);*/
 ?>
 <script>
   window.fbAsyncInit = function() {
