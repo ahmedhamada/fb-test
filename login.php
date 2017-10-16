e82b0db75cfbc413356317600eafee24868ecb32
Your Name <ahmedhamada333@gmail.com>
2017-10-16 07:02:42 +0200

    YOUR MESSAGE

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ session_start();
 <body>
 <div class="container">
     <?php if (isset($_SESSION['facebook_access_token'])): ?>
-        <center>the number of posts is: </center>
+        <center> number of posts is: </center>
     <?php else: ?>
         <h2 style="text-align: center;">know the number of posts you create on facebook</h2>
     <?php endif ?>
@@ -81,7 +81,7 @@ if (isset($accessToken)) {
         $profile_request = $fb->get('/me?fields=posts.limit(239){id}');
         $profile = $profile_request->getGraphNode()->asArray();
         
-      echo '<h3>'.$posts_count = count($profile['posts']) . 'post<h3><br>'; 
+      echo '<center> <h3>'.$posts_count = count($profile['posts']) . 'post<h3></center><br>'; 
 
     } catch(Facebook\Exceptions\FacebookResponseException $e) {
         // When Graph returns an error
