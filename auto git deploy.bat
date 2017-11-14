1bf3f4e06498299bf4272a168d405dfbe066e21c
ahmedhamada <am2627@fayoum.edu.eg>
2017-11-14 11:16:54 +0200

    YOUR MESSAGE

--- auto git deploy.bat
+++ auto git deploy.bat
@@ -1,6 +1,6 @@
-#1  git clone url.com
-#2  git config --global user.email "you@example.com"
-#3  git config --global user.name "ahmedhamada"
+::1  git clone url.com
+::2  git config --global user.email "you@example.com"
+::3  git config --global user.name "ahmedhamada"
 
 
 git add .
@@ -9,3 +9,5 @@ git pull
 
 git commit -m "YOUR MESSAGE"
 git push origin master
+
+pause
